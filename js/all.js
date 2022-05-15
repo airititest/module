44ad497bc80e461315dfb725e08d40a05ea66fc7
airititim <airititim0816@gmail.com>
2022-05-15 18:15:14 +0800

    test

--- js/all.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    var test01 = window.screen.availHeight;
-    var test02 = document.body.clientHeight;
-    var test03 = $('body').height();
-    console.log(test01 + '&' + test02 + '&' + test03);
-});
